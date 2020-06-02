17ab1e5838545f2088b06eb8125f81765fea8006
Mathilde Boivin <mboivin@student.42.fr>
2020-06-02 14:32:03 +0200

    Update comment

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -3,13 +3,13 @@
 import os
 import twint
 
-seed_hashtag = '#hashtag' # Replace value
+seed_hashtag = '#AmericaorTrump' # Replace value
 seed_hashtag = seed_hashtag.lower()
 output_path = os.path.join('..', 'data', 'tweets.csv')
 
 c = twint.Config()
 c.Hide_output = True
-c.Limit = 2000 # Change value
+c.Limit = 2200 # Replace value
 c.Store_object = True
 c.Search = seed_hashtag
 c.Store_csv = True
