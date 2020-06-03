64223a12c2df20ecc9514a679ae3dd4015846d68
Mathilde Boivin <mboivin@student.42.fr>
2020-06-03 18:42:43 +0200

    wip: dash app

---
+++ app.py
@@ -0,0 +1,38 @@
+import os
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(
+    __name__,
+    external_stylesheets=external_stylesheets
+    )
+
+app.title = 'tweetiments'
+
+server = app.server
+
+app.layout = html.Div([
+    html.H2('Twitter Sentiment Analysis'),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+        value='LA'
+    ),
+    html.Div(id='display-value')
+])
+
+@app.callback(dash.dependencies.Output('display-value', 'children'),
+              [dash.dependencies.Input('dropdown', 'value')])
+def display_value(value):
+    return 'You have selected "{}"'.format(value)
+
+if __name__ == '__main__':
+    app.run_server(
+        port=8000,
+        host='127.0.0.1',
+        debug=True
+        )
