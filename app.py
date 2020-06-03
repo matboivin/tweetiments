f37f9517a1442c1e6764e640834d3ae8fc2dc213
Mathilde Boivin <mboivin@student.42.fr>
2020-06-03 23:31:42 +0200

    Add graph to app

--- app.py
+++ app.py
@@ -3,6 +3,20 @@ import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
+
+import pandas as pd
+import plotly.graph_objs as go
+
+import datetime
+import re
+
+from textblob import TextBlob
+
+import nltk
+from nltk.probability import FreqDist
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -15,18 +29,109 @@ app.title = 'tweetiments'
 
 server = app.server
 
-app.layout = html.Div([
+app.layout = html.Div(
+    children=[
     html.H2(
         'Twitter Sentiment Analysis',
         style={
             'textAlign': 'center'
         }
+    ),
+
+    html.Div(id='update-sentiment'),
+
+    dcc.Interval(
+        id='interval-component-slow',
+        interval=1*10000, # in milliseconds
+        n_intervals=0
     )
-])
+
+    ],
+    style={'padding': '20px'}
+)
+
+@app.callback(Output('update-sentiment', 'children'),
+              [Input('interval-component-slow', 'n_intervals')])
+
+def update_sentiment_viz(n):
+
+    # Load data
+    df_path = os.path.join('data', 'clean_tweets.csv')
+
+    parse_dates = ['date', 'time']
+    df = pd.read_csv(
+        df_path,
+        parse_dates=parse_dates
+    )
+
+    pol = lambda x: TextBlob(x).sentiment.polarity
+    df['polarity'] = df['tweet'].apply(pol)
+    df['polarity'] = df['polarity'].apply(lambda x: 1 if x > 0.1 else -1 if x < -0.1 else 0)
+
+    # Clean and transform data to enable time series
+    result = df.groupby(
+        [
+            pd.Grouper(
+                key='time',
+                freq='20s'
+            ),
+            'polarity'
+        ]
+    ).count().unstack(fill_value=0).stack().reset_index()
+
+    time_series = result['time'][result['polarity']==0].reset_index(drop=True)
+
+    # Create the graph 
+    children = [
+                html.Div([
+                    html.Div([
+                        dcc.Graph(
+                            id='crossfilter-indicator-scatter',
+                            figure={
+                                'data': [
+                                    go.Scatter(
+                                        x=time_series,
+                                        y=result['id'][result['polarity']==0].reset_index(drop=True),
+                                        name="Neutral",
+                                        opacity=0.8,
+                                        mode='lines',
+                                        line=dict(width=0.5, color='rgb(131, 90, 241)'),
+                                        stackgroup='one' 
+                                    ),
+                                    go.Scatter(
+                                        x=time_series,
+                                        y=result['id'][result['polarity']==-1].reset_index(drop=True),
+                                        name="Negative",
+                                        opacity=0.8,
+                                        mode='lines',
+                                        line=dict(width=0.5, color='rgb(255, 50, 50)'),
+                                        stackgroup='two' 
+                                    ),
+                                    go.Scatter(
+                                        x=time_series,
+                                        y=result['id'][result['polarity']==1].reset_index(drop=True),
+                                        name="Positive",
+                                        opacity=0.8,
+                                        mode='lines',
+                                        line=dict(width=0.5, color='rgb(184, 247, 212)'),
+                                        stackgroup='three' 
+                                    )
+                                ]
+                            }
+                        )
+                    ],
+                    style={'width': '73%', 'display': 'inline-block', 'padding': '0 0 0 20'}),
+
+                    ],
+
+                    style={'marginLeft': 70}
+                )
+            ]
+    return children
 
 if __name__ == '__main__':
     app.run_server(
         port=8000,
         host='127.0.0.1',
         debug=True
-        )
+    )
